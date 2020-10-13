68a51f12962caed8617d87e410518570afeea659
Krishna Penukonda <penukonda.krishna.moorthy@gmail.com>
2020-10-13 19:00:21 +0800

    1000x faster AudioClipDataset

--- beatbrain/datasets/audio.py
+++ beatbrain/datasets/audio.py
@@ -1,92 +1,124 @@
 from pathlib import Path
-import librosa
 from joblib import Parallel, delayed
-from itertools import tee
-import numpy as np
-import os
-import audioread
 from natsort import natsorted
-import mutagen
+from loguru import logger
+import soundfile as sf
+import resampy
+import numpy as np
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
 from torch.utils.data import Dataset
-import torchvision
-from torchvision import transforms
 from ..utils import registry
+
+
+def get_num_segments(path, max_segment_length, min_segment_length):
+    """
+    Calculate the number of audio segments of sufficient length contained within an audio file.
+
+    Args:
+        path: Path to a single audio file
+        max_segment_length (float): The maximum length (in seconds) of each audio segment. If `None`, 1 segment is assumed.
+        min_segment_length (float): The minimum length (in seconds) of each audio segment.
+    """
+    try:
+        file_info = sf.info(str(path))  # Load file info and check its validity
+        # Return 1 if segmenting is disabled
+        if max_segment_length is None:
+            return 1
+        # Compute the number of segments otherwise
+        else:
+            sr = file_info.samplerate
+            samples = file_info.frames
+            duration = samples / sr
+            num_segments = int(duration / max_segment_length)
+            if duration % max_segment_length >= min_segment_length:
+                num_segments += 1
+            return num_segments
+    except RuntimeError:  # SoundFile raises a `RuntimeError` when it fails to read a file :(
+        return 0
 
 
 @registry.register("dataset", "AudioClipDataset")
 class AudioClipDataset(Dataset):
-    def __init__(
-        self,
-        directory,
-        audio_load_options=None,
-        clip_duration=4,
-        transform=None,
-    ):
+    def __init__(self, paths, max_segment_length=10, min_segment_length=4, sample_rate=22050, mono=True):
+        """
+        Args:
+            paths: A path or a collection of paths (file or directory). If a collection of paths is provided, they must all be of the same type.
+            max_segment_length (float): The maximum length (in seconds) of each audio segment.
+            min_segment_length (float): The minimum length (in seconds) of each audio segment. Shorter segments are discarded.
+            sample_rate (int): The rate at which to resample audio. If `None`, no resampling is performed.
+            mono (bool): Whether to downmix multichannel audio clips to a single channel.
+        """
         super().__init__()
-        self.directory = Path(directory)
-        self.audio_load_options = {"sr": 22050, "res_type": "kaiser_fast"}
-        if audio_load_options:
-            self.audio_load_options.update(audio_load_options)
-        self.transform = transform or self.default_transform
-        self.clip_duration = clip_duration
+        # Store params
+        self.max_segment_length = max_segment_length
+        self.min_segment_length = min_segment_length
+        self.sample_rate = sample_rate
+        self.mono = mono
 
-        self.tracks = self._get_files()
-        self.track_durations = self._get_durations(self.tracks)
-        self.cumulative_track_durations = np.cumsum(self.track_durations)
-        self.track_starts = np.roll(self.cumulative_track_durations, 1)
-        self.track_starts[0] = 0
-        print(f"Found {self.cumulative_track_durations[-1]} seconds of audio")
+        # Scan for files
+        try:  # Single file or directory
+            paths = Path(paths)
+            self.paths = list(paths.iterdir()) if paths.is_dir() else [paths]
+        except TypeError:  # Collection of files
+            self.paths = list(map(Path, paths))
+        self.paths = np.asarray(natsorted(self.paths))
 
-    def _get_files(self):
-        dir_contents = natsorted(map(str, self.directory.rglob("*")))
-        files = list(filter(os.path.isfile, dir_contents))
-        return files
+        # Count the number of segments in each audio file
+        self.num_track_segments = np.array(Parallel(n_jobs=-1)(delayed(get_num_segments)(str(path), self.max_segment_length, self.min_segment_length) for path in self.paths))
+        # Find and exclude unusable tracks (either unreadable or too short)
+        valid_tracks_mask = self.num_track_segments > 0
+        invalid_tracks_mask = ~valid_tracks_mask
+        if invalid_tracks_mask.sum() > 0:
+            logger.warning(f"Failed to load {invalid_tracks_mask.sum()} tracks.")
+            for invalid_index in invalid_tracks_mask.nonzero()[0]:
+                logger.debug(f"Failed to load {str(self.paths[invalid_index])}")
+            self.paths = self.paths[valid_tracks_mask]
+            self.num_track_segments = self.num_track_segments[valid_tracks_mask]
+        self.cumulative_num_track_segments = np.cumsum(self.num_track_segments)
+        self.num_total_segments = self.cumulative_num_track_segments[-1]
 
-    def _get_durations(self, files):
-        def get_duration(f):
-            try:
-                # Loads metadata only, but unsure of filetype support
-                mutagen_file = mutagen.File(f)
-                if not mutagen_file:
-                    raise mutagen.MutagenError
-                return mutagen_file.info.length
-            except mutagen.MutagenError:
-                # Probably loads the entire file before length can be calculated
-                with audioread.audio_open(f) as audio:
-                    return audio.duration
+    def __getitem__(self, index):
+        """
+        Fetches an audio segment as a numpy array.
+        Reads a segment of audio, performs resampling/downmixing to mono
 
-        durations = Parallel(n_jobs=-1, backend="threading")(
-            delayed(get_duration)(f) for f in files
-        )
-        # Truncate track lengths to multiples of clip duration
-        durations = [(d // self.clip_duration) * self.clip_duration for d in durations]
-        return durations
+        Args:
+            index (int): The index of the segment to fetch.
 
-    def __getitem__(self, key):
-        if key < 0:
-            raise IndexError(f"Negative index not supported (got {key})")
-        clip_start = key * self.clip_duration
-        # print("Clip start point:", clip_start)
-        if clip_start >= self.cumulative_track_durations[-1]:
-            raise IndexError(f"Clip index {key} is out of bounds in clip list with {len(self)} elements.")
-        track_index = np.argmax(self.cumulative_track_durations > clip_start)
-        # print("Track index:", track_index)
-        clip_index = int((clip_start - self.track_starts[track_index]) / self.clip_duration)
-        # print("Clip index in track:", clip_index)
-        track, sr = librosa.load(self.tracks[track_index], **self.audio_load_options)
-        clip = track[sr * clip_index: sr * (clip_index + self.clip_duration)]
-        # print("Clip length:", len(clip))
-        if self.transform:
-            clip = self.transform(clip)
-        return clip
+        Returns:
+            tuple: A 2D `np.float32` array of raw audio, and the audio's sample rate
+        """
+        if index < 0:
+            index = self.num_total_segments + index
+        if index >= len(self):
+            raise IndexError(f"Sample index out of range. Max index is {len(self) - 1}")
+        track_index = np.min(np.where(self.cumulative_num_track_segments > index))
+        if track_index == 0:
+            index_remainder = index
+        else:
+            index_remainder = index - self.cumulative_num_track_segments[track_index - 1]
+        track_path = self.paths[track_index]
+        with sf.SoundFile(str(track_path)) as file:
+            track_sample_rate = file.samplerate
+            start_pos = track_sample_rate * self.max_segment_length * index_remainder
+            num_samples = track_sample_rate * self.max_segment_length
+
+            # Load raw audio
+            file.seek(start_pos)
+            audio = file.read(num_samples, dtype=np.float32, fill_value=0, always_2d=True)
+            audio = audio.T  # Channel-first index order
+
+            # Resample if required
+            if self.sample_rate is None or self.sample_rate == track_sample_rate:
+                output_sr = track_sample_rate
+            else:
+                output_sr = self.sample_rate
+                audio = resampy.resample(audio, track_sample_rate, output_sr, filter="kaiser_fast")
+            # Optionally convert to mono
+            if self.mono and audio.ndim > 1:
+                audio = audio.mean(0, keepdims=True)
+            # Convert to tensor
+            return audio, output_sr
 
     def __len__(self):
-        return int(self.cumulative_track_durations[-1] / self.clip_duration)
-
-    @property
-    def default_transform(self):
-        return transforms.Compose([transforms.Lambda(torch.from_numpy),])
+        return self.num_total_segments
